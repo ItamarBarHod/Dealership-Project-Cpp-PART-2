18bf407f94b86ac6c72d34d70977b6b3ddceeecf
myHeapsDontLie <itamarbarhod@gmail.com>
2023-05-12 19:59:18 +0300

    last

--- CarDealership/SuperBoatCar.h
+++ CarDealership/SuperBoatCar.h
@@ -9,11 +9,11 @@ private:
 
 protected:
 	void toOs(std::ostream& out) const;
+	void setPrice() override;
 
 public:
 	SuperBoatCar(const char* companyName, COLOR color, float price, int drivingSpeed, unsigned numOfWheels, int sailingSpeed, unsigned numOfSails);
 
-	void setPrice() override;
 	int getSpeed() const override; // MAX(sailspeed, driveSpeed)
 	float getMaxSpeed() const; // turbo * getSpeed
 };
